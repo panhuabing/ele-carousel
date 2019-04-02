aab37604646b9ccd9dc6cf8ee8f7677f781e8935
phb <3122263036@qq.com>
2019-04-02 17:57:03 +0800

    新增json文件

--- src/main.js
+++ src/main.js
@@ -4,12 +4,20 @@ import Vue from 'vue'
 import App from './App'
 // import router from './router'
 import ElementUI from 'element-ui'
+
 import 'element-ui/lib/theme-chalk/index.css'
+
 import axios from 'axios'
+
+import VueResource from 'vue-resource'
+
+Vue.prototype.$http = axios
+// Vue.use(VueResource);
+
 
 Vue.use(ElementUI);
 
-Vue.prototype.$http = axios
+
 
 Vue.config.productionTip = false
 
